07d5c71777dab24664c13c6f8c0a8db222f5a44d
Cathrin Senst <cathrin.senst@gmail.com>
2020-11-27 15:42:19 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='curl-rainbow',
+    packages=find_packages(),
+    version='0.1',
+    # license='MIT',
+    description='Curl for Atari',
+    # author='Michael Laskin',
+    # author_email = 'lucidrains@gmail.com',
+    url='https://github.com/aravindsrinivas/curl_rainbow',
+    keywords=['self-supervised learning', 'artificial intelligence'],
+    install_requires=[
+        # 'torch>=1.6',
+        # 'kornia>=0.4.0'
+    ],
+    classifiers=[
+        # 'Development Status :: 4 - Beta',
+        # 'Intended Audience :: Developers',
+        # 'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        # 'License :: OSI Approved :: MIT License',
+        # 'Programming Language :: Python :: 3.6',
+    ],
+)
